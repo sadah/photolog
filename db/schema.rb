3f275d19eed8b26ff6c415485873ce6844d19d51
sadah <sadaaki.hirai@gmail.com>
2015-09-26 23:18:05 +0900

    change column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926131525) do
+ActiveRecord::Schema.define(version: 20150926140509) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "image"
@@ -24,13 +24,13 @@ ActiveRecord::Schema.define(version: 20150926131525) do
   add_index "photos", ["user_id"], name: "index_photos_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider",                            null: false
+    t.string   "provider"
     t.string   "uid",                                 null: false
     t.string   "nickname",                            null: false
     t.string   "avatar_url",                          null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
